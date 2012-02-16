6e21d26661e07b6496c3aca748fda15e5b5615ea
Ryan Kennedy <ryan@lazygenius.me>
2012-02-16 16:46:07 +0000

    Commented the code...

--- web.js
+++ web.js
@@ -1,3 +1,4 @@
+// Function to generate a random string/token.
 function randomString() {
     var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
     var string_length = 8;
@@ -11,16 +12,24 @@ function randomString() {
     return rs;
 }
 
+// Include express.
 var express = require('express');
 
+// Start server.
 var app = express.createServer();
 
+// Default route.
 app.get('/', function(request, response) {
 	data = "converse :: cherry.reflex.so";
 	response.send(data);
 });
 
+// Message timeline.
 app.get('/messages', function(request, response) {
+	// Set access control.
+	response.header("Access-Control-Allow-Origin", "*");
+	
+	// Intialise database class.
 	var Db = require('mongodb').Db,
     Connection = require('mongodb').Connection,
     Server = require('mongodb').Server;
@@ -31,23 +40,35 @@ app.get('/messages', function(request, response) {
 	limit = request.param('limit');	
 	messages = new Array();
 
+	// Set limit if not present.
 	if(!limit) {
-		limit = 5;
+		limit = 4;
 	}
 	
 	db.open(function(error, db) {
 		if (error) throw error;
 		db.collection('messages', function(err, collection) {
-			collection.find({}, {'limit': limit}, function(err, cursor) {
+			collection.find({}, function(err, cursor) {
+				// Sort by "when" descending.
+				cursor.sort({when: -1}).limit(limit);
+				// Iterate over results.
 				cursor.each(function(err, item) {
 					if(!item) {
-						// Do nothing.
+						// End of buffer.
+						
+						// Let's convert this to JSON.
 						data = JSON.stringify(messages);
 						
+						// Close database connection.
 						db.close();
+						
+						// Output response.
 						response.send(data);
 					} else {
+						// Push message to buffer.
 						messages[i] = item;
+						
+						// Increment i
 						i = i + 1;
 					}
 				});
@@ -57,9 +78,14 @@ app.get('/messages', function(request, response) {
 });
 
 app.get('/register', function(request, response) {
+	// Set access header.
+	response.header("Access-Control-Allow-Origin", "*");
+	
+	// Retrieve username and set token.
 	user = request.param("username");
 	token = randomString();
 	
+	// Intialize database.
 	var Db = require('mongodb').Db,
 	         Connection = require('mongodb').Connection,
 	         Server = require('mongodb').Server;
@@ -71,25 +97,35 @@ app.get('/register', function(request, response) {
 		db.collection('users', function(err, collection) {
 			collection.find({'user':user}, function(err, cursor) {
 				cursor.toArray(function(err, docs) {
+					// Does this user exist?
 					if(docs.length > 0) {
+						// Yes! Let's fail this registration attempt.
+						// Build return object.
 						handle = new Object();
 						handle.status = "failure";
 						handle.reason = "exists";
 						
+						// Convert to JSON.
 						data = JSON.stringify("0");
 					} else {
+						// User does not exist, let's register it.
 						collection.insert({'user':user, 'token':token, 'method':"restful"});
 						
+						// Build return object.
 						handle = new Object();
 						handle.status = "success";
 						handle.user = user;
 						handle.token = token;
 						handle.method = "restful";
 								
+						// Convert to JSON.
 						data = JSON.stringify(handle);
 					}
 					
+					// Close database handle.
 					db.close();
+					
+					// Send response.
 					response.send(data);
 				});
 			});
@@ -98,10 +134,18 @@ app.get('/register', function(request, response) {
 });
 
 app.get('/send', function(request, response) {
+	// Set access header.
+	response.header("Access-Control-Allow-Origin", "*");
+	
+	// Retrieve query variables.
 	user = request.param("username");
 	token = request.param("token");
 	message = request.param("message")
 	
+	// Get current time.
+	time = new Date();
+	
+	// Initialize database.
 	var Db = require('mongodb').Db,
     Connection = require('mongodb').Connection,
     Server = require('mongodb').Server;
@@ -117,36 +161,57 @@ app.get('/send', function(request, response) {
 						// Do nothing.
 					} else {
 						if(item.token != token) {
+							// Did this user have the right token?
+							
+							// Build return object.
 							handle = new Object();
 							handle.status = "failure";
 							handle.reason = "token";
 							
+							// Convert to JSON.
 							data = JSON.stringify(handle);
 							
+							// Close database handle.
 							db.close();
+							
+							// Send response.
 							response.send(data);
 						} else {
 							db.collection('messages', function(err, c) {
-								c.insert({'user':user, 'message':message, 'method':"restful"});
+								// Insert message in to data store.
+								c.insert({'user':user, 'message':message, 'method':"restful", when: time});
 
+								// Build return object.
 								handle = new Object();
 								handle.status = "success";
 								
+								
+								// Convert to JSON.
 								data = JSON.stringify(handle);
 								
+								// Close database handle.
 								db.close();
+								
+								// Send response.
 								response.send(data);
 							});
 						}
 					}
 					
+					// Does this user exist? Guess not.
+					
+					// Build return object.
 					handle = new Object();
 					handle.status = "failure";
 					handle.reason = "user";
 					
+					// Convert to JSON.
 					data = JSON.stringify(handle);
 					
+					// Close database handle.
 					db.close();
+					
+					// Send response.
 					response.send(data);
 				});
 			});
@@ -154,7 +219,10 @@ app.get('/send', function(request, response) {
 	});
 });
 
+// Let's set the port we want to listen on.
 var port = process.env.PORT || 3000;
+
+// Start the server!
 app.listen(port, function() {
   //console.log("Web: " + port);
 });
